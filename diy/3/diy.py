94e245b89f7a7fd9fc359bee4694afd17696dfce
wrgoldstein <williamrgoldstein@gmail.com>
2021-04-13 12:06:59 -0400

    Add basic config and materialization

--- diy/3/diy.py
+++ diy/3/diy.py
@@ -8,20 +8,21 @@ class Engine:
     current = None
     graph = collections.defaultdict(lambda: dict(deps=[], compiled=None, config={}))
     env = jinja2.Environment(
-        loader=jinja2.FileSystemLoader("")
+        loader=jinja2.FileSystemLoader(""),
+        finalize=lambda x: x if x is not None else ''
     )
 
     def ref(node: str):
         Engine.graph[Engine.current]["deps"].append(node)
         return node
 
-    def config(node: str):
-        Engine.graph[Engine.current[""]]
+    def config(**config: dict):
+        Engine.graph[Engine.current]["config"] = config
 
     def render(path: pathlib.Path):
         Engine.current = path.stem
         t = Engine.env.from_string(path.read_text())
-        Engine.graph[path.stem]["compiled"] = t.render()
+        Engine.graph[path.stem]["compiled"] = t.render().strip()
 
     def add_macro(path: pathlib.Path):
         m = Engine.env.from_string(path.read_text())
@@ -29,7 +30,7 @@ class Engine:
         for macro in macros:
             Engine.env.globals[macro] = getattr(m.module, macro)
 
-    env.globals.update(ref=ref)
+    env.globals.update(ref=ref, config=config)
 
     def get_sorted():
         graph = { k:v["deps"] for k,v in Engine.graph.items() }
@@ -40,27 +41,25 @@ class Engine:
 for macro in pathlib.Path("macros").glob("**/*.sql"):
     Engine.add_macro(macro)
 
+
 for model in pathlib.Path("models").glob("**/*.sql"):
     Engine.render(model)
-    
-
-expected = """
-select 
-case
-  when trim(maybe_blank)  = '' then null
-  else maybe_blank
-end from public.baz""".strip()
 
 
-compiled = Engine.graph["bar"]["compiled"]
+mat = Engine.graph["foo"]["config"]["materialized"]
+assert mat == "view"
 
-assert expected == compiled
 
 # from psycopg2 import connect
-
 # con = connect(...)
-# for node in Engine.get_sorted():
-#     sql = Engine.graph[node]["compiled"]
-#     con.execute(f"""
-#     create table {node} as ({sql})
-#     """)
+
+for node in Engine.get_sorted():
+    sql = Engine.graph[node]["compiled"]
+    mat = Engine.graph[node]["config"].get("materialized", "table")
+    stmt = f"""
+    drop {mat} if exists {node};
+    create {mat} {node} as ({sql});
+    """
+    # with con.cursor() as cur:
+    #     cur.execute(stmt)
+    print(stmt)
--- diy/3/models/foo.sql
+++ diy/3/models/foo.sql
@@ -1,1 +1,4 @@
+{{ config(materialized="view") }}
+
+
 select * from {{ ref("bar") }}
